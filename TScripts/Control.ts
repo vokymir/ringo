fbf59508498373826b0b4aacea2a8f500bbae30e
Voky <voky.consultant@gmail.com>
2024-05-10 23:16:06 +0200

    create blank control class

---
+++ TScripts/Control.ts
@@ -0,0 +1,29 @@
+
+class Control{
+
+    constructor(){
+
+    }
+
+    static createTournament(){
+        // new tournament
+        // buildTournamentPage
+    }
+
+    static loadTournament(){
+        // load
+        // build
+    }
+
+    static resumeTournament(){
+        // build (from local storage)
+    }
+
+    static justMatch(){
+        // build just match
+    }
+
+    // maybe rather UI
+    // match popup after save - should close the page and go to tournament?
+    // match popup at the presumed end of match - should save and goback to tournament?
+}
