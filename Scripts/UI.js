a4e918a22f3a9ee29c27e562a3d9d3744ec50092
actions-user <action@github.com>
2024-05-11 13:05:33 +0000

    build: 5e022a02f27b21d826ec2dcdd06fa39d5f05a925

--- Scripts/UI.js
+++ Scripts/UI.js
@@ -1,1 +1,19 @@
 "use strict";
+class UI {
+    static buildHomePage() {
+        // append all buttons and whatever
+    }
+    static buildTournamentPage() {
+        // append all buttons with current tournament inserted to html
+    }
+    static buildMatchPage() {
+        // as before, insert specific match to html if needed
+    }
+    static buildJustMatchPage(match) {
+        // build a page for it
+        // add a button for: save and start a tournament from it
+    }
+    static updateTournamentPage() {
+        // basically when change of order method happen, so the ui reflects it
+    }
+}
